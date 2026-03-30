721b89c69b3698d0a4e50014e1288dd8458ede26
assistantk <mayankorai1200@gmail.com>
2026-03-31 01:09:42 +0530

    Fix invalid token in Products.Designer.cs: replace stray '<' with XML doc for Dispose

--- stockmangmentsystem/Products.Designer.cs
+++ stockmangmentsystem/Products.Designer.cs
@@ -2,9 +2,7 @@
 {
     partial class Products
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
+        
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
